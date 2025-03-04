03abacf5c587dd4b4ebb4326c78ea7b2fbe5a6fc
krisave <kavere4@lsu.edu>
2025-03-04 16:30:59 -0600

    creating a text for field for the user to enter the room number

--- pft_flutter_hunt/lib/main.dart
+++ pft_flutter_hunt/lib/main.dart
@@ -118,26 +118,69 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-class KristenPage extends StatelessWidget{
+class KristenPage extends StatefulWidget {
   const KristenPage({super.key});
+
+  @override
+  _KristenPageState createState() => _KristenPageState();
+}
+
+class _KristenPageState extends State<KristenPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController myController = TextEditingController();
+
+   @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  void _checkInput() {
+    if (myController.text == "2215") {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return const AlertDialog(
+            content: Text("Correct! You've entered 2215."),
+          );
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 50, 128, 148),
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 53, 106, 67),
-        title: Text("Kristen's Page")),
-        body: Stack(
-          children: [
-            Positioned.fill(
-              child: Image.asset(
-                'assets/driving_sim.jpeg',
-                fit: BoxFit.cover,
-              )
-            )
-          ],
-        ),
+        backgroundColor: const Color.fromARGB(255, 53, 106, 67),
+        title: const Text("Kristen's Page"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextFormField(
+              controller: myController,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Enter the room number!',
+              ),
+            ),
+          ),
+          Expanded(
+            child: Image.asset(
+              'assets/driving_sim.jpeg',
+              fit: BoxFit.cover,
+              width: double.infinity,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _checkInput, // Call function to check input
+        tooltip: 'Check Number',
+        child: const Icon(Icons.check),
+      ),
     );
   }
 }
