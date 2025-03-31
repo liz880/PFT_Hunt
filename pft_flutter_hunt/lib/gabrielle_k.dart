2fd4d03bad7eaa8cc01cbb1997b741f0a759a4e8
Gabrielle King <gking16@lsu.edu>
2025-03-30 22:20:48 -0500

    Alter setup test for navigation to 2nd floor locations

--- pft_flutter_hunt/lib/gabrielle_k.dart
+++ pft_flutter_hunt/lib/gabrielle_k.dart
@@ -15,125 +15,125 @@ class StudyState extends State<StudyRoomsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor:
-                const Color(0xFF3C1053),
-            title: const Text('Study Rooms', style: TextStyle(
-                color:  Color(
-                    0xFFD29F13),),),
-            bottom: const TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.info)),
-                Tab(icon: Icon(Icons.question_answer)),
-              ],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF3C1053),
+          title: const Text(
+            'Study Rooms',
+            style: TextStyle(
+              color: Color(0xFFD29F13),
             ),
           ),
-          body: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/gek_study.png"),
-                fit: BoxFit.cover,
-                colorFilter: ColorFilter.mode(
-                    Colors.black.withOpacity(0.40), BlendMode.dstATop),
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.info)),
+              Tab(icon: Icon(Icons.question_answer)),
+            ],
+          ),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/gek_study.png"),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.40),
+                BlendMode.dstATop,
               ),
             ),
-            child: TabBarView(
-              children: [
-                // Children for "Info" tab
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      color: Colors.white70,
-                      width: 450,
-                      height: 330,
-                      alignment: Alignment.center,
-                      padding: EdgeInsets.all(16.0),
-                      child: Center(
-                        child: Text(
-                          "You will often see students wandering around for a good place to hunker down and study, and those in the know will first check these study rooms tucked away in the 2100 wing of the second floor. Don't be surprised if you see all of them filled if you don't get there early enough, especially when exam season rolls around.",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
+          ),
+          child: TabBarView(
+            children: [
+              // Info Tab content
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    color: Colors.white70,
+                    width: 450,
+                    height: 330,
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(16.0),
+                    child: Center(
+                      child: Text(
+                        "You will often see students wandering around for a good place to hunker down and study, and those in the know will first check these study rooms tucked away in the 2100 wing of the second floor. Don't be surprised if you see all of them filled if you don't get there early enough, especially when exam season rolls around.",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
                         ),
                       ),
                     ),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              const Color(0xFF3C1053),
-                          foregroundColor:
-                              const Color(0xFFD29F13),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF3C1053),
+                      foregroundColor: const Color(0xFFD29F13),
+                    ),
+                    child: const Text('Back'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+              // Question Tab content
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    color: Colors.white70,
+                    width: 450,
+                    height: 150,
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(16.0),
+                    child: Center(
+                      child: Text(
+                        "What is the week before finals called when these rooms will most likely be used?",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
                         ),
-                        child: const Text('Back'),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      color: Colors.white70,
-                      width: 450,
-                      height: 150,
-                      alignment: Alignment.center,
-                      padding: EdgeInsets.all(16.0),
-                      child: Center(
-                        child: Text(
-                          "What is the week before finals called when these rooms will most likely be used?",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
                       ),
                     ),
-                    OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                          backgroundColor:
-                              stateA ? Colors.redAccent : Colors.white70),
-                      onPressed: () {
-                        setState(() {
-                          stateA = !stateA;
-                        });
-                      },
-                      child: Text('a) Tears Week'),
+                  ),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: stateA ? Colors.redAccent : Colors.white70,
                     ),
-                    OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                          backgroundColor:
-                              stateB ? Colors.redAccent : Colors.white70),
-                      onPressed: () {
-                        setState(() {
-                          stateB = !stateB;
-                        });
-                      },
-                      child: Text('b) Hell Week'),
+                    onPressed: () {
+                      setState(() {
+                        stateA = !stateA;
+                      });
+                    },
+                    child: Text('a) Tears Week'),
+                  ),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: stateB ? Colors.redAccent : Colors.white70,
                     ),
-                    OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                          backgroundColor: stateC
-                              ? Colors.lightGreenAccent
-                              : Colors.white70),
-                      onPressed: () {
-                        setState(() {
-                          stateC = !stateC;
-                        });
-                      },
-                      child: Text("c) Dead Week"),
+                    onPressed: () {
+                      setState(() {
+                        stateB = !stateB;
+                      });
+                    },
+                    child: Text('b) Hell Week'),
+                  ),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: stateC ? Colors.lightGreenAccent : Colors.white70,
                     ),
-                  ],
-                ),
-              ],
-            ),
+                    onPressed: () {
+                      setState(() {
+                        stateC = !stateC;
+                      });
+                    },
+                    child: Text("c) Dead Week"),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
@@ -273,129 +273,124 @@ class DeanState extends State<DeanSuitePage> {
 
 class StudentServicesPage extends StatefulWidget {
   const StudentServicesPage({super.key});
-
+  
   @override
   State<StudentServicesPage> createState() => StudentServicesState();
 }
 
 class StudentServicesState extends State<StudentServicesPage> {
-
   bool stateTrue = false;
   bool stateFalse = false;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor:
-                const Color(0xFF3C1053),
-            title: const Text('Student Services Suite', style: TextStyle(
-                color: const Color(
-                    0xFFD29F13),),),
-            bottom: const TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.info)),
-                Tab(icon: Icon(Icons.question_answer)),
-                Tab(icon: Icon(Icons.image)),
-              ],
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF3C1053),
+        title: const Text(
+          'Student Services Suite',
+          style: TextStyle(
+            color: Color(0xFFD29F13),
           ),
-          body: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/gek_student.png"),
-                colorFilter: ColorFilter.mode(
-                    Colors.black.withOpacity(0.40), BlendMode.dstATop),
-                fit: BoxFit.cover,
-              ),
+        ),
+        bottom: const TabBar(
+          tabs: [
+            Tab(icon: Icon(Icons.info)),
+            Tab(icon: Icon(Icons.question_answer)),
+            Tab(icon: Icon(Icons.image)),
+          ],
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/gek_student.png"),
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.40),
+              BlendMode.dstATop,
             ),
-            child: TabBarView(
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: TabBarView(
+          children: [
+            // Info Tab content
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                // Children for "Info" tab
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      color: Colors.white70,
-                      width: 450,
-                      height: 330,
-                      alignment: Alignment.center,
-                      padding: EdgeInsets.all(16.0),
-                      child: Center(
-                        child: Text(
-                          "The Student Services Suite houses the offices for all of the College of Engineering's Counselors, though you are more likely to meet them via a zoom meeting rather than in person.",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
+                Container(
+                  color: Colors.white70,
+                  width: 450,
+                  height: 330,
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(16.0),
+                  child: Center(
+                    child: Text(
+                      "The Student Services Suite houses the offices for all of the College of Engineering's Counselors, though you are more likely to meet them via a zoom meeting rather than in person.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
                       ),
                     ),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              const Color(0xFF3C1053),
-                          foregroundColor:
-                              const Color(0xFFD29F13),
-                        ),
-                        child: const Text('Back'),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                  ],
+                  ),
                 ),
-
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      color: Colors.white70,
-                      width: 450,
-                      height: 150,
-                      alignment: Alignment.center,
-                      padding: EdgeInsets.all(16.0),
-                      child: Center(
-                        child: Text(
-                          "Is this location one of the Lost and Founds within PFT?",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                    ),
-                    OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                          backgroundColor: stateTrue
-                              ? Colors.lightGreenAccent
-                              : Colors.white70),
-                      onPressed: () {
-                        setState(() {
-                          stateTrue = !stateTrue;
-                        });
-                      },
-                      child: Text('True'),
-                    ),
-                    OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                          backgroundColor:
-                              stateFalse ? Colors.redAccent : Colors.white70),
-                      onPressed: () {
-                        setState(() {
-                          stateFalse = !stateFalse;
-                        });
-                      },
-                      child: Text('False'),
-                    ),
-                  ],
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF3C1053),
+                    foregroundColor: const Color(0xFFD29F13),
+                  ),
+                  child: const Text('Back'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                 ),
               ],
             ),
-          ),
+            // Question Tab content
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  color: Colors.white70,
+                  width: 450,
+                  height: 150,
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(16.0),
+                  child: Center(
+                    child: Text(
+                      "Is this location one of the Lost and Founds within PFT?",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: stateTrue ? Colors.lightGreenAccent : Colors.white70,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      stateTrue = !stateTrue;
+                    });
+                  },
+                  child: Text('True'),
+                ),
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: stateFalse ? Colors.redAccent : Colors.white70,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      stateFalse = !stateFalse;
+                    });
+                  },
+                  child: Text('False'),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
